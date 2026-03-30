5eb66fb685bbeafe14970252792a896bf5ef3c36
heli9x <kondwanibwenzingombe@gmail.com>
2026-03-30 19:37:52 +0200

    modified:   subtraction.py

--- subtraction.py
+++ subtraction.py
@@ -2,7 +2,6 @@ def subtraction(nums):
     difference = int(nums[0])
     for num in range(1, len(nums)):
         difference = difference - int(nums[num])
-        #print(difference, nums[num])
 
 
     return difference
