57385a4f29fb0c7df7fd85d6f489698b50e348fb
heli9x <kondwanibwenzingombe@gmail.com>
2026-03-30 21:58:44 +0200

    Refactored code for divide.py to a modular ised function version using the same logic but with one change to the parameters taken by a the program previously: program required both an operation and list of numbers now: program only requires a list of numbers
    modified main.py's CLI interface to adopt the divide funtion from divide.py
    mods: op_dict - added key "3", increased operation options in the enter_numbers function, added spaces to str_op in log_history, regrouped the if else logic to separate calculation operations from non operations e.g addition, multiplication, subtraction and division have their own block away from logging and exiting.
    
    😎✌️

--- main.py
+++ main.py
@@ -1,50 +1,51 @@
 from addition import *
 from subtraction import *
+from divide import *
 
-hist_list = []
 def choose_operation():
-    operation = input("choose operation:\n[1]-addition\n[2]-subtraction\n [9]-history\n[0]-to exit\n:")
+    operation = input("choose operation:\n[1]-addition\n[2]-subtraction\n[3]-division\n[4]-multiply\n[9]-history\n[0]-to exit\n:")
     return operation
 
 def enter_numbers():
     numbers = input("enter numbers in the following format [1 12 13] without the '[]', this is the same as 1 + 12 + 13\n:")
     return numbers.split(" ")
 
+hist_list = []
 def log_history(res, op, nums):
-    op_dict = {"1":"+", "2":"-"}
-    str_op = f"{op_dict[op]}".join(nums)
+    op_dict = {"1":"+", "2":"-", "3":"/", "4":"x"}
+    str_op = f" {op_dict[op]} ".join(nums)
     hist_str = f"""
         got: {res}
         from: {str_op}
     """
     hist_list.append(hist_str)
 
-
 def run_calculation():
     while True:
         op = choose_operation()
-        if op == "0":
-            break
-        else:
-            result = None
-            if op == "1":
-                nums = enter_numbers()
-                result = addition(nums)
-                print(f"Answer is: {result}\n")
-                log_history(result, op, nums)
-
-
-
-            elif op == "2":
-                nums = enter_numbers()
-                result = subtraction(nums)
-                print(f"Answer is: {result}\n")
-                log_history(result, op, nums)
-
+        if op in ["0", "9"]:
+            if op == "0":
+                break
             elif op == "9":
                 print("your history: ")
                 for hist in hist_list:
                     print(hist)
+
+        elif op in ["1", "2", "3", "4"]:
+            nums = enter_numbers()
+            result = None
+
+            if op == "1":
+                result = addition(nums)
+
+            elif op == "2":
+                result = subtraction(nums)
+            
+            elif op == "3":
+                result = divide(nums)
+
+            print(f"Answer is: {result}\n")    
+            log_history(result, op, nums)
 
 
 if __name__ == "__main__":
