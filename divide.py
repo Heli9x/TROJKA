1c3a71cfdb316982cee2c26e7695e87d987f9b94
Andrej <apetrovic2320@gmail.com>
2026-03-30 19:48:24 +0200

    divide

---
+++ divide.py
@@ -0,0 +1,19 @@
+sign = input()
+numbers=[]
+i=0
+while True:
+    x = input()
+    if x == '':
+        break
+    numbers.append(int(x))
+    i += 1
+m = 0
+result = 1
+if sign == '/' and i >= 2:
+    while m < i:
+        if m==0:
+            result = numbers[m]
+        else:
+            result /= numbers[m]
+        m += 1
+print(result)
